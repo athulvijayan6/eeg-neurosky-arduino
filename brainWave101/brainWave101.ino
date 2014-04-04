13e2e343a8f253243426e25ccba10c739c452858
Athul Vijayan <athulvijayan6@gmail.com>
2014-04-04 17:23:18 +0530

    plotter at 9600 baud from frontiernerds.com

--- brainWave101/brainWave101.ino
+++ brainWave101/brainWave101.ino
@@ -1,9 +1,4 @@
-#include <SoftwareSerial.h>
-
-SoftwareSerial ser(10,11);
 #include <Brain.h>
-
-
  
 // Set up the brain parser, pass it the hardware serial object you want to listen on.
 Brain brain(Serial2);
@@ -15,17 +10,7 @@ void setup() {
 }
  
 void loop() {
-    // Expect packets about once per second.
-    // The .readCSV() function returns a string (well, char*) listing the most recent brain data, in the following format:
-    // "signal strength, attention, meditation, delta, theta, low alpha, high alpha, low beta, high beta, low gamma, high gamma"   
-    //while (Serial.available()){
-        //char c = Serial.read();
-        //if (c == 'R') {
-                //Serial.println('S');
-                while (!brain.update());//brain.readCSV()
-                Serial.println(brain.readDelta());
-                //Serial.println('E');
-        //}
-    //}
+    while (!brain.update());   //wait for new packet
+    Serial.println(brain.readCSV());  //print packet
 }
 

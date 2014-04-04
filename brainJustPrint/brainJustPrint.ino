f12ba10cb374f03e4835dcd445f90184e21ffae4
Athul Vijayan <athulvijayan6@gmail.com>
2014-04-04 16:56:17 +0530

    working with RAW EEG library.-only RAW data available

--- brainJustPrint/brainJustPrint.ino
+++ brainJustPrint/brainJustPrint.ino
@@ -1,9 +1,6 @@
-#include <SoftwareSerial.h>
+#include "Brain57600.h"
 
-SoftwareSerial brain(10,11);
-
-int latestByte,lastByte;
-
+Brain brain(Serial2);
 void setup() {
     // Start the hardware serial.
     Serial2.begin(57600);
@@ -12,16 +9,6 @@ void setup() {
 }
 
 void loop() {
-   
-    while(Serial2.available()){
-        latestByte = Serial2.read();
-            if ((latestByte==170)&&(lastByte==170)){  //latestByte==170)&&(lastByte==170
-                Serial.println("sync");
-                while(Serial2.available()<2);
-                Serial.println(Serial2.read());
-                Serial.println(Serial2.read());
-                latestByte = 0;
-            }
-        lastByte = latestByte;
-    }
+    while(!brain.update()) ;
+    Serial.println(brain.readRaw());
 }
